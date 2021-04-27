4ca8c55738143c146ba3a4025ed78c79a0173a09
conroy.white <conroy.white@mastek.com>
2021-04-27 16:37:45 +0100

    initial commit

---
+++ src/test/java/com/naming/server/EurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naming.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
